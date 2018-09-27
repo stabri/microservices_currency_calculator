39ca47e61b3df4f6a9f495486e6a7fd6f13676f8
stabri <paulstarbi@gmail.com>
2018-09-27 14:05:32 +0200

    add eureka service clieent and server, swich ribon to use eureka naming server in instances checking

--- currency-conversion-serviece/src/main/java/com/paul/currencyconversionserviece/CurrencyConversionServieceApplication.java
+++ currency-conversion-serviece/src/main/java/com/paul/currencyconversionserviece/CurrencyConversionServieceApplication.java
@@ -2,10 +2,12 @@ package com.paul.currencyconversionserviece;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.paul.currencyconversionserviece")
+@EnableDiscoveryClient
 public class CurrencyConversionServieceApplication {
 
 	public static void main(String[] args) {
