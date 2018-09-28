838fed1bac79f6509ab907006a13a7bc3cb7b6f4
stabri <paulstarbi@gmail.com>
2018-09-28 10:46:39 +0200

    zuulu take proxy request between services

--- currency-conversion-serviece/src/main/java/com/paul/currencyconversionserviece/CurrencyExchangeProxy.java
+++ currency-conversion-serviece/src/main/java/com/paul/currencyconversionserviece/CurrencyExchangeProxy.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service") to use API gateaway, need to update  URI as well to zulu form host:zuluPort/appName/URI
+@FeignClient(name = "zulu-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     ConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
