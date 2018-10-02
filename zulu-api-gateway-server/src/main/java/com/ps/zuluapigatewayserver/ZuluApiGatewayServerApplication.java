bca8d4ec029a3e41d825884f4c7ced6ce842e6a4
stabri <paulstarbi@gmail.com>
2018-10-02 16:47:42 +0200

    add modules pom

--- zulu-api-gateway-server/src/main/java/com/ps/zuluapigatewayserver/ZuluApiGatewayServerApplication.java
+++ zulu-api-gateway-server/src/main/java/com/ps/zuluapigatewayserver/ZuluApiGatewayServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +13,10 @@ public class ZuluApiGatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuluApiGatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public  defaultSampler(){
+        return new ();
     }
 }
